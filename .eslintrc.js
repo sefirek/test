00c4f26a5edd3b10777a6625fec2af5e072ec31b
sefirek <sefir@o2.pl>
2022-07-20 12:03:51 +0200

    example

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb-base'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'linebreak-style': 0,
+  },
+};
