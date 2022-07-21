73bccfb8a98c7fff5dd3afc7026fb3e12794eaf8
sefirek <sefir@o2.pl>
2022-07-21 10:20:36 +0200

    dodano socket.io do serwera

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server);
 const port = process.env.PORT || 5000;
 
 app.get('/api/hello', (req, res) => {
@@ -12,4 +17,14 @@ app.post('/api/world', (req, res) => {
     `I received your POST request. This is what you sent me: ${req.body.post}`,
   );
 });
-app.listen(port, () => console.log(`Listening on port ${port}`));
+// app.listen(port, () => console.log(`Listening on port ${port}`));
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('chat message', (message) => {
+    socket.emit('chat message', 'it is ok');
+  });
+});
+
+server.listen(port, () => {
+  console.log('listening on *:3000');
+});
